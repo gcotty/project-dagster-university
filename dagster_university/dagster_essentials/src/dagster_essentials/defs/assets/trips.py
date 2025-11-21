aaf36de2bed155e662e227b51322ca8b0b43b70d
Grant Cottingham <54192928+gcotty@users.noreply.github.com>
2025-11-21 21:54:58 +0000

    dagster essentials lessons 6-8

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -3,14 +3,19 @@ import os
 import requests
 import dagster as dg
 from dagster_essentials.defs.assets import constants
-from dagster._utils.backoff import backoff
+from dagster_essentials.defs.partitions import monthly_partition
+from dagster_duckdb import DuckDBResource
 
-@dg.asset
-def taxi_trips_file() -> None:
+@dg.asset(
+        partitions_def=monthly_partition
+)
+def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
     """
         The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
     """
-    month_to_fetch = '2023-03'
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
+    
     raw_trips = requests.get(
         f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
     )
@@ -31,43 +36,49 @@ def taxi_zones_file() -> None:
         output_file.write(raw_zones.content)
 
 @dg.asset(
-    deps=["taxi_trips_file"]
+    deps=["taxi_trips_file"],
+    partitions_def=monthly_partition
 )
-def taxi_trips() -> None:
+def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
     """
-        The raw taxi trips dataset, loaded into  duckdb
-    """
-    query = """
-        create or replace table trips as (
-            select
-                VendorID as vendor_id,
-                PULocationID as pickup_zone_id,
-                DOLocationID as dropoff_zone_id,
-                RatecodeID as rate_code_id,
-                payment_type as payment_type,
-                tpep_dropoff_datetime as dropoff_datetime,
-                tpep_pickup_datetime as pickup_datetime,
-                trip_distance as trip_distance,
-                passenger_count as passenger_count,
-                total_amount as total_amount
-            from 'data/raw/taxi_trips_2023-03.parquet'
-        );
+        The raw taxi trips dataset, loaded into  duckdb partitioned by month
     """
 
-    conn = backoff(
-        fn=duckdb.connect,
-        retry_on=(RuntimeError, duckdb.IOException),
-        kwargs={
-            "database": os.getenv("DUCKDB_DATABASE")
-        },
-        max_retries=10
-    )
-    conn.execute(query)
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
+
+    query = f"""
+        create table if not exists trips (
+                vendor_id integer,
+                pickup_zone_id integer,
+                dropoff_zone_id integer,
+                rate_code_id double,
+                payment_type integer,
+                dropoff_datetime timestamp,
+                pickup_datetime timestamp,
+                trip_distance double,
+                passenger_count double,
+                total_amount double,
+                partition_date varchar
+            );
+
+        delete from trips where partition_date = '{month_to_fetch}';
+
+        insert into trips
+        select
+            VendorID, PULocationID, DOLocationID, RatecodeID, payment_type,
+            tpep_dropoff_datetime, tpep_pickup_datetime, trip_distance, passenger_count,
+            total_amount, '{month_to_fetch}' as partition_date
+        from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(query)
 
 @dg.asset(
     deps=["taxi_zones_file"]
 )
-def taxi_zones() -> None:
+def taxi_zones(database: DuckDBResource) -> None:
     """
         Create taxi zones table in duckdb
     """
@@ -82,12 +93,5 @@ def taxi_zones() -> None:
         );
     """
 
-    conn = backoff(
-        fn=duckdb.connect,
-        retry_on=(RuntimeError, duckdb.IOException),
-        kwargs={
-            "database": os.getenv("DUCKDB_DATABASE")
-        },
-        max_retries=10
-    )
-    conn.execute(query)
+    with database.get_connection() as conn:
+        conn.execute(query)
